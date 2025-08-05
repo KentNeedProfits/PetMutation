e94f1360c47906d03d479f4fd8802b6ebe62054b
KentNeedProfits <scriptsroblox690@gmail.com>
2025-08-05 22:46:46 +0800

    Update Roll Mutation.lua

--- Roll Mutation.lua
+++ Roll Mutation.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/L1m1tzz/Bruh/refs/heads/main/Breh", true))()
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/9ed5W0Jq2jgheiOK/raw", true))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/yjWnKIqGY5t8FETn/raw", true))()
